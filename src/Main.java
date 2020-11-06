574680e4c056b6240adeb4eb45ba9ec0b1c2c75e
gerald <greengorilla3@protonmail.com>
2020-11-06 13:16:49 -0800

    Created second scene. First scene now has book for instructions and button to go to second scene.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.image.*;
@@ -21,29 +22,50 @@ public class Main extends Application {
         //get image from resources.
         //https://www.tutorialspoint.com/javafx/javafx_images.htm
 
-        Image image = new Image(
+        Image jungleImage = new Image(
                 getClass().getResource("resources/jungleImage.jpg").toString()
         );
 
-        ImageView imageView = new ImageView(image);
+        Image bookImage = new Image(
+                getClass().getResource("resources/book.jpeg").toString()
+        );
 
-        imageView.setX(50);
-        imageView.setY(50);
+        ImageView jungleImageView = new ImageView(jungleImage);
+        jungleImageView.setPreserveRatio(true);
 
-        imageView.setFitHeight(455);
-        imageView.setFitWidth(500);
+        ImageView jungleImageView2 = new ImageView(jungleImage);
+        jungleImageView.setPreserveRatio(true);
 
-        imageView.setPreserveRatio(true);
+        //create the image view for the book.
+        ImageView bookImageView = new ImageView(bookImage);
+        bookImageView.setPreserveRatio(true);
+        bookImageView.setX(600);
+        bookImageView.setY(230);
+        bookImageView.setScaleX(2.0);
+        bookImageView.setScaleY(2.0);
 
-        Group root = new Group(imageView);
-        
-        Scene scene1 = new Scene(root, 600, 600);
+        //create scene for gameplay.
+        Group gameRoot = new Group(jungleImageView2);
+        Scene sceneGame = new Scene(gameRoot, 1200, 1200);
+
+        //create button that will go from menu to the gameplay
+        Button startButton = new Button("Start");
+        startButton.setLayoutX(645);
+        startButton.setLayoutY(450);
+        startButton.setOnAction(e -> {
+            window.setScene(sceneGame);
+            window.setMaximized(false);
+            window.setMaximized(true);
+        });
+
+        //create scene for start menu
+        Group menuRoot = new Group(jungleImageView, bookImageView, startButton);
+        Scene sceneMenu = new Scene(menuRoot, 1200, 1200);
 
         //set the title of the window, maximize it, and show it.
-        window.setScene(scene1);
+        window.setScene(sceneMenu);
         window.setTitle("TreeFall");
         window.setMaximized(true);
         window.show();
-
     }
 }
