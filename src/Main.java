4c54cfcc988d9fe61ea74cf0661566e1de433f04
gerald <greengorilla3@protonmail.com>
2020-11-06 16:23:57 -0800

    Added images and imageViews for other elements. Added textField and buttons for insertion, deletion, and search.

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,10 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.scene.image.*;
-
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class Main extends Application {
@@ -19,6 +18,8 @@ public class Main extends Application {
     @Override
     public void start(Stage window) throws FileNotFoundException {
 
+        BST tree = new BST();
+
         //get image from resources.
         //https://www.tutorialspoint.com/javafx/javafx_images.htm
 
@@ -30,23 +31,65 @@ public class Main extends Application {
                 getClass().getResource("resources/book.jpeg").toString()
         );
 
+        Image trunkImage = new Image(
+                getClass().getResource("resources/trunk.jpeg").toString()
+        );
+
+        Image nodeImage = new Image(
+                getClass().getResource("resources/logNode.jpeg").toString()
+        );
+
+        //create jungle background imageViewers.
         ImageView jungleImageView = new ImageView(jungleImage);
         jungleImageView.setPreserveRatio(true);
-
         ImageView jungleImageView2 = new ImageView(jungleImage);
         jungleImageView.setPreserveRatio(true);
+
+        //create trunk imageViewer
+        ImageView trunkImageViewer = new ImageView(trunkImage);
+        trunkImageViewer.setPreserveRatio(true);
+        trunkImageViewer.setScaleY(3);
+        trunkImageViewer.setScaleX(3);
+        trunkImageViewer.setY(600);
+        trunkImageViewer.setX(450);
 
         //create the image view for the book.
         ImageView bookImageView = new ImageView(bookImage);
         bookImageView.setPreserveRatio(true);
+        bookImageView.setSmooth(true);
         bookImageView.setX(600);
         bookImageView.setY(230);
         bookImageView.setScaleX(2.0);
         bookImageView.setScaleY(2.0);
 
+        //create node imageViewer
+        ImageView nodeImageView = new ImageView(nodeImage);
+
+        //create textField
+        TextField textField = new TextField();
+        textField.setTranslateY(350);
+        textField.setText("new node");
+
+        //create button to insert, delete, and search nodes.
+        Button buttonInsert = new Button("insert");
+        buttonInsert.setTranslateY(380);
+        buttonInsert.setOnAction(e -> {
+            System.out.println("insert");
+            //textField.getText();
+            //tree.initInsert(0);
+        });
+        Button buttonDelete = new Button("delete");
+        buttonDelete.setTranslateY(380);
+        buttonDelete.setTranslateX(48);
+        Button buttonSearch = new Button("search");
+        buttonSearch.setTranslateY(380);
+        buttonSearch.setTranslateX(100);
+
         //create scene for gameplay.
-        Group gameRoot = new Group(jungleImageView2);
-        Scene sceneGame = new Scene(gameRoot, 1200, 1200);
+        Group gameRoot = new Group(
+                jungleImageView2, textField, buttonInsert, buttonDelete, buttonSearch
+            );
+        Scene sceneGame = new Scene(gameRoot, 1365, 700);
 
         //create button that will go from menu to the gameplay
         Button startButton = new Button("Start");
@@ -54,18 +97,15 @@ public class Main extends Application {
         startButton.setLayoutY(450);
         startButton.setOnAction(e -> {
             window.setScene(sceneGame);
-            window.setMaximized(false);
-            window.setMaximized(true);
         });
 
         //create scene for start menu
         Group menuRoot = new Group(jungleImageView, bookImageView, startButton);
-        Scene sceneMenu = new Scene(menuRoot, 1200, 1200);
+        Scene sceneMenu = new Scene(menuRoot, 1365, 700);
 
         //set the title of the window, maximize it, and show it.
         window.setScene(sceneMenu);
         window.setTitle("TreeFall");
-        window.setMaximized(true);
         window.show();
     }
 }
